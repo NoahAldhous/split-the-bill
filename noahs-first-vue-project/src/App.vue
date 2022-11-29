5086d95d4c0176b4d0a957721c9e6e239f440607
NoahAldhous <naldhous@hotmail.co.uk>
2022-11-29 17:20:26 +0000

    initialised vue app, added initial html to app.vue

---
+++ noahs-first-vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const reactiveMessage = reactive({
+  message: "Hi World!",
+});
+
+const count = ref(0)
+</script>
+
+<template>
+  <body>
+    <section>
+      <h1>
+        {{ reactiveMessage.message }}
+      </h1>
+    </section>
+    <section>
+      <p>{{ count }}</p>
+    </section>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
