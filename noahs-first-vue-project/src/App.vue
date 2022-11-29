cfda668621f79caa59ab5e65a306dea09f22e693
NoahAldhous <naldhous@hotmail.co.uk>
2022-11-29 22:09:17 +0000

    started basic bill caluclator app

--- noahs-first-vue-project/src/App.vue
+++ noahs-first-vue-project/src/App.vue
@@ -1,26 +1,101 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 
-const reactiveMessage = reactive({
-  message: "Hi World!",
-});
+const value = ref("0");
 
-const count = ref(0)
+const numberOfGuests = ref(1);
+
+const amountToPay = ref(0);
+
+function handleClick(button) {
+  if (!value.value) {
+    value.value = "0";
+  } else if (button == "del") {
+    if (value.value === "0") {
+      value.value = "0";
+    } else {
+      value.value = value.value.slice(0, -1);
+      if (!value.value) {
+        value.value = "0";
+      }
+    }
+  } else if (value.value === "0") {
+    value.value = button;
+  } else {
+    value.value = value.value + button;
+  }
+  calculateAmountToPay();
+}
+
+function calculateAmountToPay() {
+  amountToPay.value = Number(value.value / numberOfGuests.value).toFixed(2);
+}
+
+function divideBy(number){
+  numberOfGuests.value = number;
+  calculateAmountToPay();
+}
 </script>
 
 <template>
-  <body>
-    <section>
-      <h1>
-        {{ reactiveMessage.message }}
-      </h1>
+  <body className="body">
+    <section className="heading-container">
+      <h1 className="heading">Split the Bill!</h1>
     </section>
-    <section>
-      <p>{{ count }}</p>
+    <section className="value-container">
+      <h3>To Pay : £{{ amountToPay }}</h3>
+    </section>
+    <section className="numpad-container">
+      <div className="grid-container">
+        <button @click="handleClick('1')" className="grid-item">1</button>
+        <button @click="handleClick('2')" className="grid-item">2</button>
+        <button @click="handleClick('3')" className="grid-item">3</button>
+        <button @click="handleClick('4')" className="grid-item">4</button>
+        <button @click="handleClick('5')" className="grid-item">5</button>
+        <button @click="handleClick('6')" className="grid-item">6</button>
+        <button @click="handleClick('7')" className="grid-item">7</button>
+        <button @click="handleClick('8')" className="grid-item">8</button>
+        <button @click="handleClick('9')" className="grid-item">9</button>
+        <button @click="handleClick('.')" className="grid-item">.</button>
+        <button @click="handleClick('0')" className="grid-item">0</button>
+        <button @click="handleClick('del')" className="grid-item">del</button>
+      </div>
+    </section>
+    <section className="division-container">
+      <button @click="divideBy(1)" className="division-button">1</button>
+      <button @click="divideBy(2)" className="division-button">2</button>
+      <button @click="divideBy(3)" className="division-button">3</button>
+      <button @click="divideBy(4)" className="division-button">4</button>
+      <button @click="divideBy(5)" className="division-button">5</button>
     </section>
   </body>
 </template>
 
 <style scoped>
-
+.count-button {
+  color: blue;
+}
+.body {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.numpad-container {
+  height: 50%;
+  width: 50%;
+}
+.grid-container {
+  border: 1px solid red;
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+}
+.grid-item {
+  border: 1px solid blue;
+}
 </style>
