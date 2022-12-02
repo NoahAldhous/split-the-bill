20f47b6d3b3c9f7bceb36a0eddfb11ca7b60c48d
NoahAldhous <naldhous@hotmail.co.uk>
2022-12-02 11:24:37 +0000

    added clip path circle to calculator buttons

--- noahs-first-vue-project/src/App.vue
+++ noahs-first-vue-project/src/App.vue
@@ -222,7 +222,7 @@ function calculateAmountToPay() {
 .grid-item {
   border: none;
   background-color: var(--dark-color);
-  /* clip-path: circle(); */
+  clip-path: circle();
   border-radius: 50%;
   margin: 5%;
   color: var(--soft-color);
