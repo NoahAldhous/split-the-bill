cc2971f5c032389337d538702b5a9c1e050407c3
NoahAldhous <naldhous@hotmail.co.uk>
2023-01-05 12:02:05 +0000

    installed jQuery and added code snippet to prevent zoom in on double tap on touch devices

--- noahs-first-vue-project/src/App.vue
+++ noahs-first-vue-project/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import jQuery from "jquery";
 
 const userInput = ref("0");
 
@@ -10,6 +11,25 @@ const numberOfGuests = ref(1);
 const amountToPay = ref(0);
 
 const serviceCharge = ref(0);
+
+(function($) {
+  var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
+  $.fn.nodoubletapzoom = function() {
+    if (IS_IOS)
+      $(this).bind('touchstart', function preventZoom(e) {
+        var t2 = e.timeStamp
+          , t1 = $(this).data('lastTouch') || t2
+          , dt = t2 - t1
+          , fingers = e.originalEvent.touches.length;
+        $(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
+      });
+  };
+})(jQuery);
 
 function handleChange() {
   let slider = document.getElementsByClassName("slider")[0];
