b255cb11bed562f8160839a339831317955aa538
NoahAldhous <naldhous@hotmail.co.uk>
2023-01-05 12:21:38 +0000

    edited function to disabled double click zooming on touch devices

--- noahs-first-vue-project/src/App.vue
+++ noahs-first-vue-project/src/App.vue
@@ -12,25 +12,24 @@ const numberOfGuests = ref(1);
 const amountToPay = ref(0);
 
 const serviceCharge = ref(0);
-$(document).ready(function(){
-  (function($) {
-    var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
-    $.fn.nodoubletapzoom = function() {
-      if (IS_IOS)
-        $(this).bind('touchstart', function preventZoom(e) {
-          var t2 = e.timeStamp
-            , t1 = $(this).data('lastTouch') || t2
-            , dt = t2 - t1
-            , fingers = e.originalEvent.touches.length;
-          $(this).data('lastTouch', t2);
-          if (!dt || dt > 500 || fingers > 1) return; // not double-tap
 
-          e.preventDefault(); // double tap - prevent the zoom
-          // also synthesize click events we just swallowed up
-          $(this).trigger('click').trigger('click');
+$(document).ready(function(){
+  var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
+  $.fn.nodoubletapzoom = function() {
+    if (IS_IOS)
+      $(this).bind('touchstart', function preventZoom(e) {
+        var t2 = e.timeStamp
+          , t1 = $(this).data('lastTouch') || t2
+          , dt = t2 - t1
+          , fingers = e.originalEvent.touches.length;
+        $(this).data('lastTouch', t2);
+        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+        e.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        $(this).trigger('click').trigger('click');
         });
     };
-  })(jQuery);
 });
 
 function handleChange() {
